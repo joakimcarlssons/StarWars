7b9f54e1a31769d3dda12bb19ef72440d1b0acca
Joakim Carlsson <hej@joakimcarlsson.se>
2021-01-14 15:02:13 +0100

    Added loaders. Fixed responsivity.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let currentMin = 1;
-let currentMax = 6;
+let currentMax = 8;
 let currentPage = 1;
 let currentCharacter = 1;
-let charsPerPage = 6
+let charsPerPage = 8
 let maxPage = 0;
 let charValues = ["height", "mass", "hair_color", "skin_color", "birth_year", "gender"]
 let planetValues = ["rotation_period", "orbital_period", "diameter", "climate", "gravity", "terrain"]
@@ -17,16 +17,14 @@ async function getData(url) {
 // Fill character details
 function fillDetails(url) {
     getData(url).then(data => {
-        console.log(data)
-
-        document.querySelector(".top > h2").innerText = data["name"]
+        document.querySelector(".top h2").innerText = data["name"]
     
         for(let v in charValues) {
             document.querySelector(`.${charValues[v]}`).innerText = data[`${charValues[v]}`]
         }
 
         getData(data["homeworld"]).then(planet => {
-            document.querySelector(".bottom > h2").innerText = planet["name"]
+            document.querySelector(".bottom h2").innerText = planet["name"]
     
             for(let v in planetValues) {
                 document.querySelector(`.${planetValues[v]}`).innerText = planet[`${planetValues[v]}`]
@@ -34,14 +32,6 @@ function fillDetails(url) {
         })
     })
 }
-
-// Get number of characters
-let charCount;
-fetch("https://swapi.dev/api/people/")
-   .then(p => p.json())
-   .then(d => charCount = d["count"])
-   .then(() => console.log(charCount))
-
 
 // Get all characters
 function getCharacters(min, max) {
@@ -66,6 +56,8 @@ function getCharacters(min, max) {
 
         // Add lick event to character
         li.addEventListener("click", () => {
+            showLoader(".top")
+            showLoader(".bottom")
             fillDetails(`https://swapi.dev/api/people/${i}/`)
         })
 
@@ -94,9 +86,11 @@ leftArr.addEventListener("click", () => {
         return;
     }
     else {
-        currentMin -= 6
-        currentMax -= 6
+        currentMin -= charsPerPage
+        currentMax -= charsPerPage
         currentPage -= 1
+
+        showLoader(".character-list")
         getCharacters(currentMin, currentMax)
     }
 })
@@ -106,17 +100,50 @@ rightArr.addEventListener("click", () => {
         return;
     }
     else {
-        currentMin += 6
-        currentMax += 6
+        currentMin += charsPerPage
+        currentMax += charsPerPage
         currentPage += 1;
-    
+
+        showLoader(".character-list")
         getCharacters(currentMin, currentMax)
     }
 })
 
+function docReady(){
+    if(document.readyState === "complete") {
+        document.querySelectorAll(".loader").remove()
+    }
+}
+
+
+function showLoader(item) {
+    document.querySelector(`${item} .loader-wrapper`).classList.remove("hide")
+    document.querySelector(`${item} .content-wrapper`).classList.add("hide")
+
+    setTimeout(() => {
+        document.querySelector(`${item} .loader-wrapper`).classList.add("hide")
+        document.querySelector(`${item} .content-wrapper`).classList.remove("hide")
+    }, 300)
+}
+
+
+// On load
 getCharacters(currentMin, currentMax);
 
+document.onreadystatechange = function() {
+    let loaders = document.querySelectorAll(".loader-wrapper")
+    let contents = Array.from(document.querySelectorAll(".content-wrapper"))
+    
+    for (let l of loaders) { l.classList.remove("hide") }
+    for (let c of contents) { c.classList.add("hide") }
 
+    if (document.readyState === "complete") { 
+        setTimeout(() => {
+            for (let l of loaders) { l.classList.add("hide") }
+            for (let c of contents) { c.classList.remove("hide") }
+        }, 1000)
+    } 
+};
 
 
 
